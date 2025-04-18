a5301ecb20818dd5d64df9e7fef920c2dc08de30
Gabriel <mauriciorjgabriel@gmai.com>
2025-04-18 13:24:35 -0300

    Atualizando arquivos modificados

--- src/data/products.tsx
+++ src/data/products.tsx
@@ -15,7 +15,7 @@ export const products: Product[] = [
     name: " Kit com 13 PLACAS 3D 77CM X 70CM",
     brand: "Fácil de Aplicar",
     price: "R$ 110,50",
-    image: "/placa.png",
+    image: "/placa1.png",
      description:"Transforme qualquer ambiente com essas placas autoadesivas 3D em estilo tijolinho branco! Fáceis de aplicar e com resultado incrível. Ideal pra dar aquele up na sua parede sem bagunça!",
      features: [
       "Tamanho de cada placa: 77cm x 70cm",
